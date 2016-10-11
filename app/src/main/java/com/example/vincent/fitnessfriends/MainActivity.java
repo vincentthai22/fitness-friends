a0d0f1a3fb85ed4130369732c3894c2b484e9fc3
J Granata <jjgranata@cpp.edu>
2016-10-10 20:44:27 -0700

    Added an expandable list as our skeleton. See documentation and issues for more info

--- app/src/main/java/com/example/vincent/fitnessfriends/MainActivity.java
+++ app/src/main/java/com/example/vincent/fitnessfriends/MainActivity.java
@@ -1,13 +1,115 @@
 package com.example.vincent.fitnessfriends;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    expanderSkeleton liAdapt;
+    ExpandableListView liView;
+    List<String> liHead;
+    HashMap<String, List<String>> liChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        liView = (ExpandableListView) findViewById(R.id.lvExp);
+
+
+        makeLists();
+
+        liAdapt = new expanderSkeleton(this, liHead, liChild);
+
+
+        liView.setAdapter(liAdapt);
+
+
+        liView.setOnGroupClickListener(new OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+
+                return false;
+            }
+        });
+
+        liView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        liHead.get(groupPosition) + " Minimized",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        liView.setOnGroupExpandListener(new OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        liHead.get(groupPosition) + " Inflated",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        liView.setOnChildClickListener(new OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        liHead.get(groupPosition)
+                                + " : "
+                                + liChild.get(
+                                liHead.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
+    }
+
+    private void makeLists() {
+        liHead = new ArrayList<>();
+        liChild = new HashMap<>();
+
+        // Add option data
+        liHead.add("profile");
+        liHead.add("friends");
+        liHead.add("data");
+
+        // Add more sub-options here
+        List<String> profileOptions = new ArrayList<>();
+        profileOptions.add("op 1");
+        profileOptions.add("op 2");
+
+        List<String> friendsOptions = new ArrayList<>();
+        friendsOptions.add("op 1");
+
+        List<String> dataOptions = new ArrayList<>();
+        dataOptions.add("op 2");
+
+
+        liChild.put(liHead.get(0), profileOptions);
+        liChild.put(liHead.get(1), friendsOptions);
+        liChild.put(liHead.get(2), dataOptions);
     }
 }
