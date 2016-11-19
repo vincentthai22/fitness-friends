5e5d1fe229c6367f347320383376735a81c09888
J Granata <jjgranata@cpp.edu>
2016-11-18 17:54:49 -0800

    Started adding textboxes for the routine page; can also be used for diet. Strings.xml contains the nexessary strings for the textboxes and generic_textbox has the xml code.

--- app/src/main/java/com/example/vincent/fitnessfriends/models/Routine.java
+++ app/src/main/java/com/example/vincent/fitnessfriends/models/Routine.java
@@ -1,8 +1,39 @@
 package com.example.vincent.fitnessfriends.models;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 /**
  * Created by Vincent on 10/25/2016.
  */
 
-public class Routine {
+public class Routine extends Activity {
+
+        EditText edit;
+        Button button;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.generic_textbox);
+
+            edit = (EditText) findViewById(R.id.edittext);
+            button = (Button) findViewById(R.id.button);
+
+            button.setOnClickListener(new OnClickListener() {
+
+                public void onClick(View v) {
+                    String str = edit.getText().toString();
+                    Toast msg = Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG);
+                    msg.show();
+                }
+
+            });
+
+        }
 }
